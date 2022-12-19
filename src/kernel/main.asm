43fb0a8a80265018c06ef537c3f8d42f570bd3a6
7y1o <tdesu@vk.com>
2022-12-19 03:57:05 +0300

    Removed useless parts of code

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,16 +1,22 @@
-org 0x7C00              ; set code start address
+org 0x0                 ; set code start address
 bits 16                 ; emit 16-bit code
 
 %define ENDL 0x0D, 0x0A ; define line end code
 
 start:                  ; entry point
-    jmp main
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
 
 ; Prints a string to the screen. Params:
 ;   - ds:si points to the string
 puts:                   
     push si
     push ax
+    push bx
 
 .loop:
     lodsb               ; loads next character in al
@@ -18,33 +24,16 @@ puts:
     jz .done            ; finish the work if next character is null
 
     mov ah, 0x0e        ; set TTY write function
+    mov bh, 0
     int 0x10            ; call video interrupt
     
     jmp .loop           ; call loop to write next character
 
 .done:                  ; loop is finished
+    pop bx
     pop ax
     pop si
     ret
-
-main:                   ; main program section
-    ; ------------------- setup data segments
-    mov ax, 0           ; can`t write to DS/ES directly
-    mov ds, ax
-    mov es, ax
-
-    ; ------------------- setup stack
-    mov ss, ax
-    mov sp, 0x7C00      ; stack grows downwards from where are loaded in mem
-
-    ; ------------------- print message
-    mov si, msg_hello
-    call puts
-
-    hlt
-
-.halt:                  ; CPU halting section
-    jmp .halt
 
 msg_hello: db 'Hello, World!', ENDL, 0
 
